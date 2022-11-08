54ce2b229e4d29a5229f034815dc4ad09dfd9cc6
Tu-Wei_Chen <asd415305@gmail.com>
2022-11-08 11:19:14 +0800

    + Add Polynomial Operation for Add

--- Polynomial/main.c
+++ Polynomial/main.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 
 
 typedef struct PolyNode_T *polynomial;
+
+typedef struct NONZERO_TERM *nonzero_term;
+
 typedef struct
 {
     int coef;
     int expon;
 }NONZERO_TERM;
+
 typedef struct PolyNode_T
 {
     polynomial link;
@@ -27,6 +32,19 @@ NONZERO_TERM polynomailSeq[5] =
     {.coef = 20, .expon = 0},
 
 };
+NONZERO_TERM polynomailSeq2[5] =
+{
+    {.coef = 5, .expon = 11},
+    {.coef = 1, .expon = 8},
+    {.coef = 5, .expon = 6},
+    {.coef = 100, .expon = 2},
+    {.coef = 20, .expon = 0},
+
+};
+
+nonzero_term MakePolynomialArray(char * str);
+
+
 
 // Easy Make Polynomail
 polynomial PolyInital(NONZERO_TERM* pPolySeq, int Size)
@@ -73,17 +91,93 @@ void PrintfPolynomail(polynomial head)
 
 }
 
+polynomial AddPolynomial(polynomial head, polynomial head2)
+{
+    polynomial head3;
+    polynomial last, current;
+    last = malloc(sizeof(POLYNODE_T));
+    (last->term).coef = 0;
+    (last->term).expon = INT_MAX;
+    head3 = last;
+
+    // Compare two order sequence
+    while (head != NULL && head2 != NULL)
+    {
+        int compVal1, compVal2;
+        compVal1 = (head->term).expon;
+        compVal2 = (head2->term).expon;
+        current = malloc(sizeof(POLYNODE_T));
+        last->link = current;
+        last = current;
+
+        if (compVal1 > compVal2)
+        {
+            (current->term).coef = (head->term).coef;
+            (current->term).expon = compVal1;
+            head = head->link;
+        }
+        else if (compVal2 > compVal1)
+        {
+            (current->term).coef = (head2->term).coef;
+            (current->term).expon = compVal2;
+            head2 = head2->link;
+        }
+        else
+        {
+            (current->term).coef = ((head->term).coef << 1);
+            (current->term).expon = compVal1;
+            head = head->link;
+            head2 = head2->link;
+
+        }
+    }
+
+    if (head != NULL)
+    {
+        ;
+
+    }
+    else
+    {
+        head = head2;
+    }
+
+    while (head != NULL)
+    {
+        current = malloc(sizeof(POLYNODE_T));
+        last->link = current;
+        last = current;
+        (current->term).coef = (head->term).coef;
+        (current->term).expon = (head->term).expon;
+        head = head->link;
+    }
+
+    current->link = NULL;
+    last = head3;
+    head3 = head3 ->link;
+    last->link = NULL;
+    free(last);
+    return head3;
+
+
+
+}
+
 int main(void)
 {
 
     //PolyNode_T array;
     //PolyNode_T* head1 = malloc(sizeof(PolyNode_T));
     //head1->coef = 5;
-    polynomial head2;
+    polynomial head2, head3, head4;
     head2 = PolyInital(polynomailSeq, 5);
+    head3 = PolyInital(polynomailSeq2, 5);
 
     printf("%dx^%d\n", (head2->term).coef, (head2->term).expon);
     PrintfPolynomail(head2);
+    PrintfPolynomail(head3);
+    head4 = AddPolynomial(head2, head3);
+    PrintfPolynomail(head4);
 
     free(head2);
 
