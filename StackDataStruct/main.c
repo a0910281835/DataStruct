bf03be76061ae12b61e4a6fc58a523e390e4f63e
Wei-Chen Tu <asd415305@gmail.com>
2022-11-18 07:01:53 +0800

    * Practice StackPointer DataStruct

--- StackDataStruct/main.c
+++ StackDataStruct/main.c
@@ -47,24 +47,118 @@ int PopStack(PSTACK_ARRAY_T pStackArray)
 
     }
 }
+typedef int ELEMENT_T;
+typedef struct StackNode *STACKNODE_PT;
+
+typedef enum
+{
+    NODE,
+    HEAD,
+    TAIL
+
+}TYPE;
+
+typedef enum
+{
+    EMPTY,
+    SUCC
+}CELL_SIZE;
+
+typedef struct StackNode
+{
+    ELEMENT_T val;
+    STACKNODE_PT pNext;
+    TYPE type;
+
+}StackNode;
+
+typedef struct RETURN
+{
+    ELEMENT_T val;
+    CELL_SIZE result;
+}RETURN;
+
+STACKNODE_PT CreatePtrStack(void)
+{
+    STACKNODE_PT head = (STACKNODE_PT) malloc(sizeof(StackNode));
+    head->type = HEAD;
+    STACKNODE_PT tail = (STACKNODE_PT) malloc(sizeof(StackNode));
+    tail->type = TAIL;
+    head->pNext = tail;
+    tail->pNext = head;
+    return head;
+}
+
+void PushStackPointer(STACKNODE_PT head, ELEMENT_T val)
+{
+    STACKNODE_PT pNode = (STACKNODE_PT) malloc(sizeof(StackNode));
+    pNode->val = val;
+    pNode->type = NODE;
+    pNode->pNext = head->pNext;
+    head->pNext = pNode;
+
+}
+
+RETURN PopStackPointer(STACKNODE_PT head)
+{
+    RETURN ret;
+    STACKNODE_PT pNode = head->pNext;
+
+    if (TAIL == pNode->type)
+    {
+        ret.result = EMPTY;
+        printf("the stack is empty\n");
+    }
+    else
+    {
+        head->pNext = pNode->pNext;
+        ret.val = pNode->val;
+        free(pNode);
+        pNode = NULL;
+        ret.result = SUCC;
+        return ret;
+    }
+}
+
 int main(void)
 {
-    PSTACK_ARRAY_T pStackArray = CreatStackArray();
-    printf("%d\n", pStackArray->size);
-    PopStack(pStackArray);
-
+    STACKNODE_PT head = CreatePtrStack();
+    printf("Head Type:%d\n", (int)(head->type));
+    STACKNODE_PT pNode = head->pNext;
+    printf("TAIL Type:%d\n", pNode->type);
     int idx = 0;
-    for (idx = -4; idx < 4; idx++)
+    for (idx = 0; idx < 20; idx++)
     {
-        int val = idx * idx + 3;
-        printf("push stack val : %d\n", val);
-        PushStack(pStackArray, val);
+        PushStackPointer(head, idx);
     }
+    RETURN ret;
+    do
+    {
+        ret = PopStackPointer(head);
+        printf("pop Val %d\n", ret.val);
 
-    for (idx = pStackArray->size; idx >= 0; idx--)
-    {
-        int val = PopStack(pStackArray);
-        printf("pop stack order val : %d\n", val);
-    }
+    }while(SUCC == ret.result);
+
+    //PSTACK_ARRAY_T pStackArray = CreatStackArray();
+    //printf("%d\n", pStackArray->size);
+    //PopStack(pStackArray);
+
+    //int idx = 0;
+    //for (idx = -4; idx < 4; idx++)
+    //{
+    //    int val = idx * idx + 3;
+    //    printf("push stack val : %d\n", val);
+    //    PushStack(pStackArray, val);
+    //}
+
+    //for (idx = pStackArray->size; idx >= 0; idx--)
+    //{
+    //    int val = PopStack(pStackArray);
+    //    printf("pop stack order val : %d\n", val);
+    //}
+
+
+
+
     return 1;
 }
