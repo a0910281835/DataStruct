c15f40591c8d77c1be2384bfdab1dfc82d1a8833
Wei-Chen Tu <asd415305@gmail.com>
2023-04-06 12:02:14 +0800

    + Add leetcode

--- Tree/tree_application.c
+++ Tree/tree_application.c
@@ -1492,3 +1492,29 @@ int** zigzagLevelOrder(struct TreeNode* root, int* returnSize, int** returnColum
     return collect;
 
 }
+
+
+
+//104. Maximum Depth of Binary Tree
+//Given the root of a binary tree, return its maximum depth.
+//A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+//Example 1:
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 3
+//
+//         3
+//      /     \
+//     9      20
+//           /  \
+//          15   7
+
+
+
+
+
+int maxDepth(struct TreeNode* root)
+{
+    int 
+    if (NULL == root) return 0;
+
+}
