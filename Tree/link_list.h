92b95ff2f4e9c19a9940588a9760804603a68f21
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2023-11-19 22:14:38 +0800

    * Define LRU strcut with double link list and hash table

--- Tree/link_list.h
+++ Tree/link_list.h
@@ -41,11 +41,55 @@ T_THREAD * PopWaittingQueue(T_WAITTING_PRIORITY_QUEUE *pWattingQueue);
 // leetcode 146 LRU algorithm
 //
 // hashing and double link list
+
+//  Define Node Struct and operation
+typedef struct T_CACHE_NODE * PT_CACHE_NODE;
+typedef struct T_CACHE_NODE
+{
+    int key;
+    int value;
+    PT_CACHE_NODE pPrev;
+    PT_CACHE_NODE pNext;
+    PT_CACHE_NODE pConflictNext;
+
+}T_CACHE_NODE;
+
+PT_CACHE_NODE CreateNode(int key, int value);
+void DeleteNode(PT_CACHE_NODE pNode);
+
+
+
+// Double link list with operation
+typedef struct T_DOUBLE_LINK_LIST * PT_DOUBLE_LINK_LIST;
+typedef struct T_DOUBLE_LINK_LIST
+{
+    PT_CACHE_NODE pHead;
+    PT_CACHE_NODE pTail;
+    int size;
+
+}T_DOUBLE_LINK_LIST;
+
+T_DOUBLE_LINK_LIST * CreateDoubleList(void);
+bool IsDoubleListEmpty(T_DOUBLE_LINK_LIST * pFifo);
+bool IsDoubleListFull(T_DOUBLE_LINK_LIST * pFifo);
+void InsertInDoubleList(T_DOUBLE_LINK_LIST * pFifo, PT_CACHE_NODE pNode);
+PT_CACHE_NODE PopInDoubleList(T_DOUBLE_LINK_LIST * pFifo);
+
+
+typedef struct T_CACHE_NODE ** PT_HASH_TABLE;
+PT_CACHE_NODE FindHashTable(PT_HASH_TABLE pHashTable, int key);
+void InsetHashTable(PT_HASH_TABLE pHashTable, int key, int value);
+
+
+
+
 typedef struct LRUCache
 {
-
-
+    PT_DOUBLE_LINK_LIST pDoublist;
+    PT_HASH_TABLE pHashTable;
 }T_LRU_CACHE;
+
+typedef struct LRUCache LRUCache;
 
 LRUCache* lRUCacheCreate(int capacity);
 int lRUCacheGet(LRUCache* obj, int key);
